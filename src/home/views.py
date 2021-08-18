fd59c98d26a64b9f85dc1664a91d2a4608814ffa
npaillasson <nathanpaillasson3@gmail.com>
2021-08-18 19:02:30 +0200

    creation of a first template

--- src/home/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def index(request):
-    return HttpResponse('<h1>Bonjour est bienvenue sur le site!</h1>')
---
+++ src/review/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from review.views import index
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        print("lzlzld,sldn", view)
+        self.assertEquals(view.func, index)
+
+# Create your tests here.
