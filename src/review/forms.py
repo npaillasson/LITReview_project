ecd056bbb5ad457948e0ab68aa932006c858ee58
npaillasson <nathanpaillasson3@gmail.com>
2021-08-25 17:07:10 +0200

    optimization of the new ticket page

---
+++ src/review/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Ticket
+
+
+class NewTicketForm(forms.ModelForm):
+    message = forms.CharField(widget=forms.Textarea(), max_length=4000)
+    title = forms.CharField(max_length=128)
+    #user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #image = forms.ImageField(null=True, blank=True)
+    #time_created
+
+    class Meta:
+        model = Ticket
+        fields = ['title', 'message', 'description', 'image']
--- src/review/views.py
+++ src/review/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from .forms import NewTicketForm
+from django.contrib.auth.models import User
 
 def index(request):
     print(request.user.is_authenticated)
     return render(request, 'review/index.html')
 
 def new_ticket(request):
+    #if request.method == 'POST':
+    #    form = NewTicketForm(request.POST)
     print(request.POST)
+    print(User.objects.first())
     return render(request, 'review/new_ticket.html')
 
