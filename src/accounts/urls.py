1d8942445a9feb75a7bb176bfc97628c5a7b4bf9
npaillasson <nathanpaillasson3@gmail.com>
2021-09-06 19:32:16 +0200

    registration and disconnection works

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import new_account, auth
 
 urlpatterns = [
     path('new_account/', new_account, name='new_account'),
     path('auth/', auth, name='auth'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
 def new_account(request):
-    form = UserCreationForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('review:index')
+    else:
+        form = UserCreationForm
     return render(request, 'accounts/new_account.html', {'form': form})
 
 def auth(request):
