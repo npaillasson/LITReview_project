2ff5335ee560913c7ba8b077b83eeffdf92af3d1
npaillasson <nathanpaillasson3@gmail.com>
2021-09-06 15:32:42 +0200

    code correction

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
 
 def new_account(request):
-    return render(request, 'accounts/new_account.html')
+    form = UserCreationForm
+    return render(request, 'accounts/new_account.html', {'form': form})
 
 def auth(request):
     return render(request, 'accounts/auth.html')
