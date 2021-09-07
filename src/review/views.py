8e070e80aad64c0016e5274c9ca665fb570cc1b1
npaillasson <nathanpaillasson3@gmail.com>
2021-09-07 15:43:52 +0200

    new tickets are now registered in the database

--- src/review/views.py
+++ src/review/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import NewTicketForm
 from django.contrib.auth.models import User
 from .forms import NewTicketForm
 
 def index(request):
-    print(request.user.is_authenticated)
     return render(request, 'review/index.html')
 
 def new_ticket(request):
-    #if request.method == 'POST':
-    #    form = NewTicketForm(request.POST)
-    #print(request.POST)
-    #print(User.objects.first())
-    print(request.POST)
-    return render(request, 'review/new_ticket.html', {'form': NewTicketForm, 'page': request.path})
+
+    user = User.objects.first()
+    if request.method == 'POST':
+        form = NewTicketForm(request.POST)
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.user = user
+            ticket.save()
+            return redirect('review:index')
+    else:
+        form = NewTicketForm
+    return render(request, 'review/new_ticket.html', {'form': form, 'page': request.path})
 
 
 
