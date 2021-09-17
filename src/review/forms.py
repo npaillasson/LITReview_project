74f8ae31773c67b1f6cb3b8ddccb17c5bec77562
npaillasson <nathanpaillasson3@gmail.com>
2021-09-17 14:21:37 +0200

    updated html code and modified the layout of the radio buttons in the review creation form

--- src/review/forms.py
+++ src/review/forms.py
@@ -3,6 +3,7 @@ from .models import Ticket, Review
 
 
 class NewTicketForm(forms.ModelForm):
+    title = forms.CharField(label='titre')
 
     class Meta:
         model = Ticket
@@ -11,8 +12,9 @@ class NewTicketForm(forms.ModelForm):
 
 class NewReviewForm(forms.ModelForm):
     choices = [(0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')]
-    body = forms.CharField(widget=forms.Textarea, max_length=8192)
-    rating = forms.TypedChoiceField(widget=forms.RadioSelect(), choices=choices, coerce=int)
+    body = forms.CharField(label='critique', widget=forms.Textarea, max_length=8192)
+    headline = forms.CharField(label='titre')
+    rating = forms.TypedChoiceField(label='note',widget=forms.RadioSelect(), choices=choices, coerce=int)
 
     class Meta:
         model = Review
